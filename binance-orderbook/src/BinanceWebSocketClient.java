748056692620445091ea270eef8a06598b374448
adambalcerzyk <adam890115@gmail.com>
2025-06-12 15:17:23 +0200

    Add WebSocket client for Binance updates

--- binance-orderbook/src/BinanceWebSocketClient.java
+++ binance-orderbook/src/BinanceWebSocketClient.java
@@ -1,3 +1,37 @@
+import java.net.URI;
+import java.util.*;
+import java.util.concurrent.CountDownLatch;
+import javax.websocket.*;
+
+@ClientEndpoint
 public class BinanceWebSocketClient {
-    
+    private final String symbol;
+    private final OrderBook orderBook;
+    private Session session;
+
+    public BinanceWebSocketClient(String symbol, OrderBook orderBook) {
+        this.symbol = symbol;
+        this.orderBook = orderBook;
+    }
+
+    public void connect() {
+        try {
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            container.connectToServer(this, new URI("wss://fstream.binance.com/ws/" + symbol + "@depth@100ms"));
+            new CountDownLatch(1).await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @OnMessage
+    public void onMessage(String message) {
+        Map<String, Object> parsed = JsonUtils.parseJson(message);
+        long updateId = ((Number) parsed.get("u")).longValue();
+
+        List<List<String>> bids = (List<List<String>>) parsed.get("b");
+        List<List<String>> asks = (List<List<String>>) parsed.get("a");
+
+        orderBook.applyUpdate(updateId, bids, asks);
+    }
 }
